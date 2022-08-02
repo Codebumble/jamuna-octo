f49e1ec8543550a8ae6c0b26f80c09cf381f2193
mdsaifurrahmann <md.saifurrahmann029@gmail.com>
2022-08-02 22:32:32 +0600

    fucking bug

--- resources/frontend/App.vue
+++ resources/frontend/App.vue
@@ -1,14 +1,12 @@
 <template>
-	<div>
-		<h1 class="text-white">Jamuna Group is Coming soon!</h1>
-
-	</div>
+  <div>
+    <h1 class="text-white">Jamuna Group is Coming soon!</h1>
+    <i class="fab fa-facebook"></i>
+  </div>
 </template>
 
-
 <script>
-
 export default {
-	setup() {},
+  setup() {},
 };
 </script>
--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="description" content="{{ env("APP_NAME")}} is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+        <meta name="keywords" content="admin template, codebumble, {{ env("APP_NAME")}}, dashboard template, flat admin template, responsive admin template, web app">
+        <meta name="author" content="Codebumble Inc.">
+        <title>@yield('title') - {{ env("APP_NAME")}}</title>
+        <link rel="apple-touch-icon" href="{{asset('images/ico/apple-icon-120.png')}}">
+        <link rel="shortcut icon" type="image/x-icon" href="{{asset('images/logo/favicon.ico')}}">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="{{asset(mix('fonts/font-awesome/css/font-awesome.min.css'))}}">
         <link rel="stylesheet" href="{{asset('frontend/css/app.css')}}">
     </head>
     <body class="antialiased">
