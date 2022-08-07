29952aee2c4fd01272cfa6196737f7d4448d2f48
mdsaifurrahmann <md.saifurrahmann029@gmail.com>
2022-08-08 03:50:11 +0600

    live chat added

--- resources/frontend/app.js
+++ resources/frontend/app.js
@@ -1,20 +1,20 @@
-require('./bootstrap');
+require("./bootstrap");
 
-import './assets/scss/app.scss';
-import './assets/js/script.js';
-import router from './router/router';
-import { createHead } from "@vueuse/head"
+import "./assets/scss/app.scss";
+import "./assets/js/script.js";
+import router from "./router/router";
+import { createHead } from "@vueuse/head";
 // import { createMetaManager } from 'vue-meta'
 
-const head = createHead()
+const head = createHead();
 
-import {
-    createApp
-} from "vue";
+import { createApp } from "vue";
+import VueTidio from "vue-tidio";
 
 import FrontEnd from "./App.vue";
 
 createApp(FrontEnd)
-    .use(router)
-    .use(head)
-    .mount("#app");
+	.use(router)
+	.use(head)
+	.use(VueTidio, { appKey: "1yoerye8srqedbbeqdfekccvmrn30ff6" })
+	.mount("#app");
