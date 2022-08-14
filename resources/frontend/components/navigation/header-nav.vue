5ed84ee1e6acc22edd046243672bbbfe80a270c7
mdsaifurrahmann <md.saifurrahmann029@gmail.com>
2022-08-14 20:59:44 +0600

    hoor added to nav

--- resources/frontend/components/navigation/header-nav.vue
+++ resources/frontend/components/navigation/header-nav.vue
@@ -90,6 +90,20 @@
 										</div>
 									</router-link>
 								</li>
+								<li class="mega-items hover:bg-indigo-50">
+									<router-link
+										to="/hoor"
+										class="mega-link">
+										<div class="flex items-center">
+											<div>
+												<h1
+													class="text-base text-gray-800">
+													Hoor
+												</h1>
+											</div>
+										</div>
+									</router-link>
+								</li>
 
 								<li class="mega-items hover:bg-green-50">
 									<router-link
