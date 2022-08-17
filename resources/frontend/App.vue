6405bacb23f9d294b37035f2297212308189c723
mdsaifurrahmann <md.saifurrahmann029@gmail.com>
2022-08-17 21:52:44 +0600

    router transition added

--- resources/frontend/App.vue
+++ resources/frontend/App.vue
@@ -1,8 +1,28 @@
 <template>
 	<appHeader />
-	<router-view></router-view>
+	<router-view v-slot="{ Component, route }">
+		<transition
+			name="fade"
+			mode="out-in">
+			<div :key="route.name">
+				<component :is="Component"></component>
+			</div>
+		</transition>
+	</router-view>
 	<appFooter />
 </template>
+
+<style lang="scss">
+	.fade-enter-from,
+	.fade-leave-to {
+		opacity: 0;
+	}
+
+	.fade-enter-active,
+	.fade-leave-active {
+		transition: opacity 0.5s ease-out;
+	}
+</style>
 
 <script>
 	import { defineAsyncComponent } from 'vue';
