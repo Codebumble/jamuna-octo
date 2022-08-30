1a1151a489fd51000e24d96c40d93c2632f7450f
Shakil Ahmed <shakilofficial0@gmail.com>
2022-08-30 11:38:34 +0600

    typo updated

--- app/Http/Controllers/FrontPage.php
+++ app/Http/Controllers/FrontPage.php
@@ -248,14 +248,14 @@ class FrontPage extends Controller
 
         if(isset($data[0])){
 
-            if(isset(json_decode($data[0]->json_date)->new_center)){
-                if(json_decode($data[0]->json_date)->new_center == "no"){
+            if(isset(json_decode($data[0]->json_data)->new_center)){
+                if(json_decode($data[0]->json_data)->new_center == "no"){
                     return json_encode(['error' =>1, 'data'=> 'Not A Media center']);
                 }
             } else {
                 return json_encode(['error' =>1, 'data'=> 'Not A Media center']);
             }
-            
+
             $data_2 = DB::select("select * from users where json_data like '%\"DistrictCompany\":".$id."%'");
             $d = $data[0];
             $d_j = json_decode($d->json_data);
