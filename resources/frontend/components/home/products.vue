cb3b8c4306c508f4d5cdc4a7cb2c86b14f9e8942
mdsaifurrahmann <md.saifurrahmann029@gmail.com>
2022-09-02 02:57:20 +0600

    fucking visibility

--- resources/frontend/components/home/products.vue
+++ resources/frontend/components/home/products.vue
@@ -5,7 +5,7 @@
 				<h2 class="text-2xl lg:text-4xl text-gray-800 font-bold">
 					{{ groupTitle.title }}
 				</h2>
-				<p v-if="groupTitle.descVisibility == true">
+				<p v-if="groupTitle.descVisibility == 'true'">
 					{{ groupTitle.description }}
 				</p>
 			</div>
