f3f81a8dab63520e8574025c5b01d202da3ee507
mdsaifurrahmann <md.saifurrahmann029@gmail.com>
2022-09-04 02:15:28 +0600

    news center bug fix

--- resources/frontend/components/news-center/jamuna.vue
+++ resources/frontend/components/news-center/jamuna.vue
@@ -29,16 +29,16 @@
 			};
 		},
 		created() {
-			this.switcher();
+			this.xswitcher();
 			this.$watch(
 				() => this.$route.params,
 				(toParams, PreviousParams) => {
-					this.switcher();
+					this.xswitcher();
 				}
 			);
 		},
 		methods: {
-			switcher() {
+			xswitcher() {
 				if (this.$route.params.id) {
 					axios
 						.get(
@@ -47,27 +47,41 @@
 								this.$route.params.id
 						)
 						.then((response) => {
-							document.title = response.data.title
+							document.title = response.data.title;
 							const data = response.data;
-							this.mediaCenter = {
-								...this.mediaCenter,
-								ytSrc: data.ytSrc,
-								title: data.title,
-								phone: data.phone,
-								email: data.email,
-								address: {
-									officeName: data.address.officeName,
-									road: data.address.road,
-								},
-								distHeading: {
-									title: data.distHeading.title,
-									desc: data.distHeading.desc,
-								},
-							};
 
-							data.distCorres.forEach((item) => {
-								this.mediaCenter.distCorres.push(item);
-							});
+							this.mediaCenter.ytSrc = data.ytSrc;
+							this.mediaCenter.title = data.title;
+							this.mediaCenter.phone = data.phone;
+							this.mediaCenter.email = data.email;
+							this.mediaCenter.address.officeName =
+								data.address.officeName;
+							this.mediaCenter.address.road = data.address.road;
+							this.mediaCenter.distHeading.title =
+								data.distHeading.title;
+							this.mediaCenter.distHeading.desc =
+								data.distHeading.desc;
+							this.mediaCenter.distCorres = data.distCorres;
+
+							// this.mediaCenter = {
+							// 	...this.mediaCenter,
+							// 	ytSrc: data.ytSrc,
+							// 	title: data.title,
+							// 	phone: data.phone,
+							// 	email: data.email,
+							// 	address: {
+							// 		officeName: data.address.officeName,
+							// 		road: data.address.road,
+							// 	},
+							// 	distHeading: {
+							// 		title: data.distHeading.title,
+							// 		desc: data.distHeading.desc,
+							// 	},
+							// };
+
+							// data.distCorres.forEach((item) => {
+							// 	this.mediaCenter.distCorres.push(item);
+							// });
 						});
 				}
 			},
