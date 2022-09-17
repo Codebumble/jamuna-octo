b8f073b9d0e35f3f35476f3d3bd43eb55ab7d9cc
Anik Hossain <anik.wdev@gmail.com>
2022-09-18 05:18:45 +0600

    about secion (inprogress)

---
+++ resources/frontend/components/home/about.vue
@@ -0,0 +1,32 @@
+<template>
+	<section class="group-company bg-slate-50">
+		<div class="container">
+			<div class="title">
+				<h2 class="text-2xl lg:text-4xl text-gray-800 font-bold">
+					About Us
+				</h2>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate accusantium vero rerum, officia, consequuntur laudantium maiores nulla vitae eos animi cumque recusandae molestias provident autem repellat temporibus, magnam atque beatae!
+				</p>
+			</div>
+			<div class="detail">
+				<div class="image">
+					<img src="/frontend/images/contents/earth2.png" alt="earth">
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<style lang="scss">
+	@import '../../assets/scss/variables/_group-company';
+</style>
+
+<script>
+	export default {
+		data() {},
+		components: {},
+		mounted() {},
+		setup() {},
+	};
+</script>
