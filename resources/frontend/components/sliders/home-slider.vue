ab9e49c7f997cc705ccbd8ac0af1081970d5988f
Anik Hossain <anik.wdev@gmail.com>
2022-11-02 15:06:32 +0600

    home header slider now support image

--- resources/frontend/components/sliders/home-slider.vue
+++ resources/frontend/components/sliders/home-slider.vue
@@ -1,36 +1,47 @@
 <template>
-	<section
-		id="cb-bg-video"
-		class="overflow-hidden cb-hero relative">
+	<section id="cb-bg-video" class="overflow-hidden cb-hero relative">
 		<div class="zigzag_bg">
-			<Splide
-				:options="optionx"
-				aria-label="header slider">
+			<Splide :options="optionx" aria-label="header slider">
 				<SplideSlide
 					v-for="(slide, item) in sliderContents"
 					:key="item"
 					class="flex justify-start items-center">
 					<div class="image">
+						<img
+							v-if="
+								/(\.jpg|\.jpeg|\.png|\.gif)$/i.exec(slide.src)
+							"
+							:src="slide.src"
+							alt=""
+							class="w-full object-cover h-[90vh]" />
 						<video
+							v-else
 							class="w-full object-cover h-[90vh]"
 							autoplay
 							muted
 							loop>
-							<source
-								:src="slide.src"
-								type="video/mp4" />
+							<source :src="slide.src" type="video/mp4" />
 							Your browser does not support the video tag.
 						</video>
-						<!-- <img
-							:src="slide.src"
-							alt=""
-							class="w-full object-cover h-77vh" /> -->
 					</div>
 					<div class="absolute w-full z-20">
 						<div class="container flex flex-col">
 							<div class="overflow-hidden">
 								<h1
-									class="ml-8 2xl:ml-8 xl:ml-0 text-2xl lg:text-[36px] text-white font-bold capitalize leading-snug w-[80%] lg:w-[50%] text-shadow-extreme">
+									class="
+										ml-8
+										2xl:ml-8
+										xl:ml-0
+										text-2xl
+										lg:text-[36px]
+										text-white
+										font-bold
+										capitalize
+										leading-snug
+										w-[80%]
+										lg:w-[50%]
+										text-shadow-extreme
+									">
 									{{ slide.heading }}
 								</h1>
 								<!-- <p
@@ -55,38 +66,38 @@
 </template>
 
 <style lang="scss">
-	@import '../../assets/scss/variables/_hero.scss';
+@import '../../assets/scss/variables/_hero.scss';
 </style>
 
 <script>
-	export default {
-		data() {
-			return {
-				sliderContents: [],
-			};
-		},
-		mounted() {
-			axios
-				.get(window.location.origin + '/frontpage-api/video-slider')
-				.then((response) => {
-					this.sliderContents = response.data;
-				});
-		},
-		setup() {
-			const optionx = {
-				autoplay: true,
-				rewind: true,
-				perPage: 1,
-				perMove: 1,
-				pagination: true,
-				arrows: true,
-				type: 'fade',
-				interval: 3000,
-				pagination: false,
-			};
-			return {
-				optionx,
-			};
-		},
-	};
+export default {
+	data() {
+		return {
+			sliderContents: [],
+		};
+	},
+	mounted() {
+		axios
+			.get(window.location.origin + '/frontpage-api/video-slider')
+			.then((response) => {
+				this.sliderContents = response.data;
+			});
+	},
+	setup() {
+		const optionx = {
+			autoplay: true,
+			rewind: true,
+			perPage: 1,
+			perMove: 1,
+			pagination: true,
+			arrows: true,
+			type: 'fade',
+			interval: 3000,
+			pagination: false,
+		};
+		return {
+			optionx,
+		};
+	},
+};
 </script>
