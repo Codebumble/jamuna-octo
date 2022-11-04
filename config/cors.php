94bf3afdc6b9c3c41248eb9e6dea888ecf7cb5a7
Shakil <shakilofficial0@gmail.com>
2022-11-04 23:28:36 +0600

    pushing cors

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'frontpage-api/*'],
 
     'allowed_methods' => ['*'],
 
