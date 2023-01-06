5541903137e48fac239e83f124dd265ff6ce2d6c
Shakil <shakilofficial0@gmail.com>
2023-01-06 18:32:35 +0600

    arrow fix try

--- resources/views/content/site-settings/front-page-slider.blade.php
+++ resources/views/content/site-settings/front-page-slider.blade.php
@@ -241,7 +241,7 @@
                     <div class="heading-elements">
                         <ul class="list-inline mb-0">
                           <li>
-                            <a data-action="collapse"><i data-feather="chevron-down"></i></a>
+                            <a data-action="collapse"><i data-feather="chevron-up"></i></a>
                           </li>
                         </ul>
                     </div>
@@ -450,7 +450,7 @@
                     <div class="heading-elements">
                         <ul class="list-inline mb-0">
                           <li>
-                            <a data-action="collapse"><i data-feather="chevron-down"></i></a>
+                            <a data-action="collapse"><i data-feather="chevron-up"></i></a>
                           </li>
                         </ul>
                     </div>
@@ -816,7 +816,7 @@
                     <div class="heading-elements">
                         <ul class="list-inline mb-0">
                           <li>
-                            <a data-action="collapse"><i data-feather="chevron-down"></i></a>
+                            <a data-action="collapse"><i data-feather="chevron-up"></i></a>
                           </li>
                         </ul>
                     </div>
