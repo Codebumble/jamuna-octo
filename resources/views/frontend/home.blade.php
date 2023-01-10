1b408fd94843f6350f4efa9eaac029dcbc925ee8
mdsaifurrahmann <md.saifurrahmann029@gmail.com>
2023-01-11 02:22:33 +0600

    favicon bug fix

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -28,8 +28,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="apple-touch-icon" href="{{ asset('images/ico/apple-icon-120.png') }}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontend/images/ico/favicon.ico') }}">
+    <link rel="apple-touch-icon" href="{{ asset('images/logo/favicon.ico') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/logo/favicon.ico') }}">
 
     <!-- Fonts -->
 
