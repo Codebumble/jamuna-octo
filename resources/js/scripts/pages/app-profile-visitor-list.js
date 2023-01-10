76f464b309db2457e4cd626c219177b7fe8c12f3
Shakil <shakilofficial0@gmail.com>
2023-01-10 23:45:33 +0600

    Uff delete soraite khub kstw hoilo

--- resources/js/scripts/pages/app-profile-visitor-list.js
+++ resources/js/scripts/pages/app-profile-visitor-list.js
@@ -169,9 +169,7 @@ $(function () {
 				''+full['username']+'" class="dropdown-item">' +
 				feather.icons['file-text'].toSvg({ class: 'font-small-4 me-50' }) +
 				'Details</a>' +
-				'<a href="javascript:;" class="dropdown-item delete-record">' +
-				feather.icons['trash-2'].toSvg({ class: 'font-small-4 me-50' }) +
-				'Delete</a></div>' +
+				'</div>' +
 				'</div>' +
 				'</div>'
 			  );
